e6aa1be6d3138ad5b4a660f97d08fbefbd45ece1
Zhige Chen <zhigec_cpp@outlook.com>
2025-05-05 20:29:36 +0800

    Some cleanup

--- demo.cpp
+++ demo.cpp
@@ -1,14 +1,19 @@
 #include "mgil.hpp"
 
 #include <print>
-#include <vector>
 
 auto main() -> int {
     using namespace mgil;
     auto image = readImage<BMPFileIO<>>("./test1.bmp");
     if (not image) {
-        std::println("{}", std::to_underlying(image.error()));
+        std::println("Image load failed with error code {}", std::to_underlying(image.error()));
+        return 1;
     }
-    auto blurred_image = gaussianBlur(image.value().toView(), 5.);
-    writeImage<BMPFileIO<>>(blurred_image.toView(), "./sobel.bmp");
+    auto processed_image = image.value().toView() | rotate90() | transform([](Pixel<UInt8_0255, rgb_layout_t> pixel) {
+        pixel.get<red_color_t>() = 0;
+        pixel.get<green_color_t>() = 0;
+        return pixel;
+    });
+    auto concat_image = concatHorizontal(processed_image, processed_image);
+    writeImage<BMPFileIO<>>(concat_image, "./processed.bmp");
 }
--- mgil.hpp
+++ mgil.hpp
@@ -31,7 +31,6 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
-#include <version>
 
 #include "mgil.hpp"
 
